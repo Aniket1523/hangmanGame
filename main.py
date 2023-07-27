fc4ab76b0047e14d32b7aa80bac298f9e0f41e45
Aniket1523 <er.aniketkangane@gmail.com>
2023-07-27 13:44:54 +0530

    Improving user experiences.

--- main.py
+++ main.py
@@ -1,86 +1,35 @@
 import random
-
-word_list = ['aniket', 'rugved', 'ankur', 'mrunmayi']
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
+from hangman_word import word_list
+from hangman_art import stages, logo
 end_of_game = False
 number_of_lives = 6
+
+print(logo)
 chosen_word = random.choice(word_list)
 print(chosen_word)
-
 word = ["_"] * len(chosen_word)
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    if guess in word:
+        print(f"You already guessed {guess}")
+        continue;
     for i in range(len(chosen_word)):
         if guess == chosen_word[i]:
             word[i] = guess
-    print(word)
+            print(f"You guessed {guess} and that's correct")
+
 
     if guess not in chosen_word:
+        print(f"You guessed {guess} and that's incorrect. You lost one life. ")
         number_of_lives -= 1
         if number_of_lives == 0:
             end_of_game = True
             print("Looser")
 
     if "_" not in word:
-        print("You won")
+        print("********** YOU WON **********")
         end_of_game = True
-
+    print(word)
     print(stages[number_of_lives])
 
