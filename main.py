3d28a00bd0482279fcb6cc59ff9e79f8e7411f8f
Aniket1523 <er.aniketkangane@gmail.com>
2023-07-27 13:27:17 +0530

    Adding stages to hangman.

--- main.py
+++ main.py
@@ -1,6 +1,62 @@
 import random
 
 word_list = ['aniket', 'rugved', 'ankur', 'mrunmayi']
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 end_of_game = False
 number_of_lives = 6
@@ -8,15 +64,14 @@ chosen_word = random.choice(word_list)
 print(chosen_word)
 
 word = ["_"] * len(chosen_word)
-print(word)
 
 while not end_of_game:
-    print(f"lives={number_of_lives}")
     guess = input("Guess a letter: ").lower()
     for i in range(len(chosen_word)):
         if guess == chosen_word[i]:
             word[i] = guess
     print(word)
+
     if guess not in chosen_word:
         number_of_lives -= 1
         if number_of_lives == 0:
@@ -27,4 +82,5 @@ while not end_of_game:
         print("You won")
         end_of_game = True
 
+    print(stages[number_of_lives])
 
